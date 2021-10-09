dd0f2b7b6b5d66b60127ce238441ac9b8a8d8e35
ExistCode <76140889+ExistCode@users.noreply.github.com>
2021-10-09 09:52:18 +0700

    Update meet2.py

--- meet2.py
+++ meet2.py
@@ -3,22 +3,22 @@ import schedule
 import time
 import random
 
-rand_min = random.randrange(41, 49)
-rand_sec1 = random.randrange(1, 3)
-rand_sec2 = random.randrange(3, 7)
+rand_min = random.randrange(41, 49) #interval minutes you want the pyautogui to join your meet
+rand_sec1 = random.randrange(1, 3) #interval second 1
+rand_sec2 = random.randrange(3, 7) #interval second 2
 
-print(f"06:{rand_min}")
+print(f"06:{rand_min}")#schedule and pyatogui will automate in 06:xx
 print(rand_sec1, rand_sec2)
 
-def main():
+def main():#main function to join meet
     time.sleep(1)
-    pyautogui.press('win', interval=0.5)
+    pyautogui.press('win', interval=0.5)#click windows key by pyautogui
     time.sleep(1)
-    pyautogui.typewrite('chrome', interval=0.5)
+    pyautogui.typewrite('chrome', interval=0.5)#auto type chrome by pyautogui(you can change it into edge or other browser)
     time.sleep(1)
     pyautogui.press('enter')
     time.sleep(1)
-    pyautogui.typewrite('https://meet.google.com/ypc-riev-yfh?authuser=1')  # https://meet.google.com/bgg-vdcc-mgj?authuser=1
+    pyautogui.typewrite('https://meet.google.com/ypc-riev-yfh?authuser=1')  #google meet link (autotype), you can change into your google meet link
     time.sleep(1)
     pyautogui.press('enter')
     time.sleep(3)
@@ -37,7 +37,7 @@ def main():
     pyautogui.click(1900,20)
     time.sleep(2)
 
-schedule.every().day.at(f"06:{rand_min}").do(main)
+schedule.every().day.at(f"06:{rand_min}").do(main)# to schedule everyday at some fixed time
 
 while True:
     schedule.run_pending()
